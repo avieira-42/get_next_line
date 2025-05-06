5b94691e2189d29aa40ff649925af791c814d7ad
avieira-42 <vieiramendesalex@gmail.com>
2025-05-06 13:55:16 +0100

    have to test

--- get_next_line.h
+++ get_next_line.h
@@ -13,7 +13,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <unistd.h>
-# include <aio.h>
+# include <stddef.h>
 # include <stdlib.h>
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
